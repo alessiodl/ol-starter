126d20622c9de0bae0bb896b306393fcc58fc576
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2019-11-21 17:24:43 +0100

    Basic controls

--- src/js/map.js
+++ src/js/map.js
@@ -1,11 +1,35 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
-import {fromLonLat} from 'ol/proj';
+import {fromLonLat, transformExtent} from 'ol/proj';
+import {defaults as defaultControls, Control, ScaleLine, ZoomToExtent, OverviewMap} from 'ol/control';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
+const ItalyExtent = [3.691406249999991, 35.31736632923787, 22.67578124999999, 47.57652571374621];
+const WorldExtent = [-173.14453125, -43.96119063892022, 164.35546875, 73.47848507889992];
+const AbruzzoExtent = [13.018364289962841, 41.68212288096967, 14.782890505994267, 42.89478878999114];
+
+
 const map = new Map({
     target: 'map',
+    controls: defaultControls().extend([
+        new ScaleLine({ 
+            // units: 'degrees' 
+        }),
+        new OverviewMap({
+            collapsed:false,
+            layers: [
+                new TileLayer({
+                  source: new OSM()
+                })
+              ]
+        }),
+        new ZoomToExtent({
+            tipLabel: "Estensione iniziale",
+            extent: transformExtent(ItalyExtent, 'EPSG:4326', 'EPSG:3857')
+        })
+    ]),
+	
     layers: [
         new TileLayer({
             source: new OSM()
