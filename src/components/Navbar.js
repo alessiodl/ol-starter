95ffb52f63991afe71ff17ae3989b588052a6f75
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2019-11-26 17:37:52 +0100

    Panels

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Navbar = () => {
+
     return ( 
         <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-primary">
             <a className="navbar-brand" href="#">
