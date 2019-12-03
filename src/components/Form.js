718121de655e012609e339c5afeaba66d57b9c0a
Alessio Di Lorenzo <alessio.dl@gmail.com>
2019-12-03 16:18:45 +0100

    Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,11 +4,37 @@ class Form extends Component {
     state = {  }
     render() { 
         return ( 
-            <div class="row">
-                <div class="col-md-12">
-                    <p class="alert alert-secondary">Test</p>
+            <form>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <input className="form-control form-control-sm" placeholder="First Input" />
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col">
+                            <input className="form-control form-control-sm" placeholder="Second Input" />
+                        </div>
+                        <div class="col">
+                            <input className="form-control form-control-sm" placeholder="Third Input" />
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col">
+                            <button className="btn btn-sm btn-block btn-primary">
+                            <i className="fas fa-paper-plane"></i> Submit</button>
+                        </div>
+                        <div class="col">
+                            <button className="btn btn-sm btn-block btn-primary">
+                            <i className="fas fa-ban"></i> Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
         );
     }
 }
